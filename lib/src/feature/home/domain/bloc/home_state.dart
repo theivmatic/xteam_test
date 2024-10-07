395de21ee8ba93a1a4b63c0507220b74cae5ab6e
theivmatic <theivmatic@mail.ru>
2024-10-07 17:34:21 +0500

    deleted unused code

--- lib/src/feature/home/domain/bloc/home_state.dart
+++ lib/src/feature/home/domain/bloc/home_state.dart
@@ -3,29 +3,23 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeBlocState extends Equatable {
   List<TaskEntity>? get tasksLoaded;
-  List<TaskEntity>? get localTasksLoaded;
 
   @override
   List<Object?> get props => [
         tasksLoaded,
-        localTasksLoaded,
       ];
 
   HomeBlocState copyWith({
     List<TaskEntity>? tasksLoaded,
-    List<TaskEntity>? localTasksLoaded,
   });
 }
 
 final class HomeBlocInitialState extends HomeBlocState {
   @override
   final List<TaskEntity>? tasksLoaded;
-  @override
-  final List<TaskEntity>? localTasksLoaded;
 
   HomeBlocInitialState({
     this.tasksLoaded,
-    this.localTasksLoaded,
   });
 
   @override
@@ -35,21 +29,15 @@ final class HomeBlocInitialState extends HomeBlocState {
   }) =>
       HomeBlocInitialState(
         tasksLoaded: tasksLoaded != '' ? tasksLoaded ?? this.tasksLoaded : null,
-        localTasksLoaded: localTasksLoaded != ''
-            ? localTasksLoaded ?? this.localTasksLoaded
-            : null,
       );
 }
 
 final class HomeBlocLoadingState extends HomeBlocState {
   @override
   final List<TaskEntity>? tasksLoaded;
-  @override
-  final List<TaskEntity>? localTasksLoaded;
 
   HomeBlocLoadingState({
     this.tasksLoaded,
-    this.localTasksLoaded,
   });
 
   @override
@@ -59,21 +47,15 @@ final class HomeBlocLoadingState extends HomeBlocState {
   }) =>
       HomeBlocLoadingState(
         tasksLoaded: tasksLoaded != '' ? tasksLoaded ?? this.tasksLoaded : null,
-        localTasksLoaded: localTasksLoaded != ''
-            ? localTasksLoaded ?? this.localTasksLoaded
-            : null,
       );
 }
 
 final class HomeBlocLoadedState extends HomeBlocState {
   @override
   final List<TaskEntity>? tasksLoaded;
-  @override
-  final List<TaskEntity>? localTasksLoaded;
 
   HomeBlocLoadedState({
     this.tasksLoaded,
-    this.localTasksLoaded,
   });
 
   @override
@@ -83,23 +65,17 @@ final class HomeBlocLoadedState extends HomeBlocState {
   }) =>
       HomeBlocLoadedState(
         tasksLoaded: tasksLoaded != '' ? tasksLoaded ?? this.tasksLoaded : null,
-        localTasksLoaded: localTasksLoaded != ''
-            ? localTasksLoaded ?? this.localTasksLoaded
-            : null,
       );
 }
 
 final class HomeBlocErrorState extends HomeBlocState {
   @override
   final List<TaskEntity>? tasksLoaded;
-  @override
-  final List<TaskEntity>? localTasksLoaded;
 
   final String? errorMessage;
 
   HomeBlocErrorState({
     this.tasksLoaded,
-    this.localTasksLoaded,
     this.errorMessage,
   });
 
@@ -111,9 +87,6 @@ final class HomeBlocErrorState extends HomeBlocState {
   }) =>
       HomeBlocErrorState(
         tasksLoaded: tasksLoaded != '' ? tasksLoaded ?? this.tasksLoaded : null,
-        localTasksLoaded: localTasksLoaded != ''
-            ? localTasksLoaded ?? this.localTasksLoaded
-            : null,
         errorMessage:
             errorMessage != '' ? errorMessage ?? this.errorMessage : null,
       );
